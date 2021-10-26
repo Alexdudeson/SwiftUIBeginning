626a96eb1ed587591f154d792100ae5a62ec2147
Alexey Yarov <alexxd91@gmail.com>
2021-10-26 17:03:22 +0300

    Initial Commit

---
+++ SwiftUIBeginning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBeginning
+//
+//  Created by Alexey Yarov on 26/10/2021.
+//  Copyright © 2021 Alexey Yarov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
